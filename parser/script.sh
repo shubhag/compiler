8459f858ef24f1a0f137256172bc5f3a09fc6358
Shubham <shubhamagrawal094@gmail.com>
2015-03-11 22:29:28 +0530

    script file update

--- parser/script.sh
+++ parser/script.sh
@@ -2,3 +2,8 @@ grep "Reduce" < parselog.txt
 grep -o '\[[^\[]*\]' parselog2.txt > parselog3.txt
 
 grep "Reduce" < parselog.txt | grep -o '\[[^\[]*\]' > parselog3.txt
+
+grep "Reduce" < parselog.txt | grep -o '\[.*\]' | tac | sed -e 's/with /\n/g' | sed 's/^.//' | awk '{print substr($0, 0, length($0))}' > parselog3.txt 
+
+
+grep "Reduce" < parselog.txt | grep -o '\[.*\]' | tac | sed -e 's/ with /\n/g' | sed 's/^.//' | awk '{print substr($0, 0, length($0))}' > parselog3.txt 
